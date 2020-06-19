374ee2713cd4190d44fb879d53f3e4bda0a33b50
ErickJBS <a311008@uach.mx>
2020-06-19 08:36:06 -0600

    project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoInputComponent } from './components/todo-input/todo-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoItemComponent,
+    TodoListComponent,
+    TodoInputComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/models/todo.model.ts
@@ -0,0 +1,4 @@
+export interface Todo {
+    content: string,
+    date: Date
+}
